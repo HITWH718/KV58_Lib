cae3b42b5599675771221453e5b45de77707673c
LuckyMth <2817895349@qq.com>
2019-03-01 16:03:10 +0800

    删除了临时文件

--- App/src/main.c
+++ App/src/main.c
@@ -1,12 +1,32 @@
+
 #include "headfile.h"
 
-
-int main()
+int main(void)
 {
-	//初始化时钟部分
     pll_init();
-
-    return 0;
+    
+    double a = 0;
+    uint16_t b = 0;
+    
+    adc_init(ADC0_DP1);
+    ftm_pwm_init(ftm0,ftm_ch0,15000);    
+    ftm_pwm_duty(ftm0,ftm_ch0,500);
+    
+    while(1)
+    {
+        b = adc_once(ADC0_DP1,ADC_16bit);
+        a = (uint16_t)(3.3*b/65536);
+        printf("%lf",a);
+        pit_delay_ms(pit0,200);
+    }
 }
 
- 
+
+
+
+
+
+
+
+
+
